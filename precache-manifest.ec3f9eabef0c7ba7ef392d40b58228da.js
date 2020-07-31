5d7fd50a74c386040a171ac985bd91a1afc9e64f
Javier Rivera <jrivera@secureauth.com>
2020-07-30 21:55:17 -0300

    Updates

---
+++ precache-manifest.ec3f9eabef0c7ba7ef392d40b58228da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbc4d59bb35b380b54a6ebe71620796",
+    "url": "/far8-ghpages-test/index.html"
+  },
+  {
+    "revision": "086723be66592f90588a",
+    "url": "/far8-ghpages-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "49ccd281700f53d14d8d",
+    "url": "/far8-ghpages-test/static/js/2.14cbd2d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/far8-ghpages-test/static/js/2.14cbd2d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "086723be66592f90588a",
+    "url": "/far8-ghpages-test/static/js/main.68e07581.chunk.js"
+  },
+  {
+    "revision": "30e626e31de5ab042b71",
+    "url": "/far8-ghpages-test/static/js/runtime-main.dc226719.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/far8-ghpages-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
